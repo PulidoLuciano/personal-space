9846a7fd0e96d46679e42ba6746a615d91172768
Luciano Pulido <lucianonicolaspulido@gmail.com>
2026-02-21 15:04:32 -0300

    Create monorepo structure with projects initialized

---
+++ packages/core/dist/index.d.ts
@@ -0,0 +1,3 @@
+declare const hello = "Hola mundo como andan esto es el core";
+export { hello };
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/core/dist/index.js
@@ -0,0 +1,3 @@
+const hello = "Hola mundo como andan esto es el core";
+export { hello };
+//# sourceMappingURL=index.js.map
