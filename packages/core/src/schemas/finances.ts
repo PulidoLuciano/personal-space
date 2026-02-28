7d76b35ecb5a8b8ef422d28a16dfa06f5c1db1c1
Luciano Pulido <lucianonicolaspulido@gmail.com>
2026-02-28 15:22:55 -0300

    Add finance's schemas with currencies

--- packages/core/src/schemas/finances.ts
+++ packages/core/src/schemas/finances.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 import { baseSchema } from "./baseSchema.js";
 
+export const currency = z.object({
+  name: z.string().min(1, "can not be empty"),
+  symbol: z.string().min(1, "can not be empty"),
+});
+
 export const insertFinanceSchema = z.object({
   title: z.string().min(1, "can not be empty."),
   description: z.string().nullable().default(null),
@@ -12,6 +17,10 @@ export const insertFinanceSchema = z.object({
 
 export const financeSchema = insertFinanceSchema.extend(baseSchema);
 
+export const financeWithCurrencySchema = financeSchema.extend({
+  currency: currency,
+});
+
 export const insertFinanceExecutionSchema = z.object({
   amount: z.number(),
   date: z.date(),
@@ -20,6 +29,11 @@ export const insertFinanceExecutionSchema = z.object({
 });
 
 export const financeExecutionSchema = insertFinanceSchema.extend(baseSchema);
+export const financeExecutionWithCurrencySchema = financeExecutionSchema.extend(
+  {
+    currency: currency,
+  },
+);
 
 export type InsertFinance = z.infer<typeof insertFinanceSchema>;
 export type Finance = z.infer<typeof financeSchema>;
@@ -27,3 +41,7 @@ export type InsertFinanceExecution = z.infer<
   typeof insertFinanceExecutionSchema
 >;
 export type FinanceExecution = z.infer<typeof financeExecutionSchema>;
+export type FinanceWithCurrency = z.infer<typeof financeWithCurrencySchema>;
+export type FinanceExecutionWithCurrency = z.infer<
+  typeof financeExecutionWithCurrencySchema
+>;
